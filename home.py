3cdd88cd931241c3cda6792814fa01ad4183570e
Michael Menjivar Alvarado <michaelmenjivar456@gmail.com>
2025-04-29 13:14:21 -0500

    Added a bit new update

--- home.py
+++ home.py
@@ -6,23 +6,48 @@ class store:
         self.name = ""
         self.location = ""
 
-    def set_var(self, name, location):
-        if not name.strip():
-            print("Invalid enter a name")
-        else:
-            self.name = name
-        if not location.strip():
-            print("Invalid enter a location")
-        else:
-            self.location = location
+    def set_info(self):
+        #this loop will keep repeating until the string is not empty
+        while True:
+            name = input("Good morning!! which store would you want to use today?\n>>> ")    
+            if not name.strip():
+                print("Invalid enter a name")
+            else:
+                self.name = name
+                break
+        while True:
+            location = input("Which location you want to use?\n>>> ")
+            if not location.strip():
+                print("Invalid enter a location")
+            else:
+                self.location = location
+                break
     def __str__(self):
         print("Store:", self.name)
         print ("Location:", self.location)
-
+    
 
 class cart:
-    def __init__(self, product_name, quantity, receipt):
-        self.product_name = product_name
-        self.quantity = quantity
-        self.receipt = receipt
+    def __init__(self):
+        self.product_name = ""
         self.cart = {}
+        self.quantity = 0
+        self.receipt = ""
+        
+    # removes item from dictionary
+    def remove_item(self):
+        for i in self.cart:
+            list += 1
+            print(str(list) + ":", i)
+        print("enter the name of the item you want to remove\n>>> ")
+    #Adds item to dictionary
+    def add_item(self, product_name, quantity):
+        self.cart[product_name] = quantity
+        print(f"You added {quantity} of {product_name}")
+def menu():
+    print("Products as follow")
+    print("Milk: $2.50\nBread: $1.98\nEgg: $0.70\nFlour: $1.18\nOil: $4.00\nCheese: $2.68\n")
+while True:
+    s = store()
+    s.set_info()
+    menu()
